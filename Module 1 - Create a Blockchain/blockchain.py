0df45f8f54f18675bf11ff332bfc28e46ec95d35
David <dyarzebinski@carlotz.com>
2022-02-06 12:30:48 -0500

    update chain

--- Module 1 - Create a Blockchain/blockchain.py
+++ Module 1 - Create a Blockchain/blockchain.py
@@ -31,12 +31,19 @@ class Blockchain:
         
         while check_proof is False:
             hash_operation = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest()
-            if has_operation[:4] = '0000':
+            if hash_operation[:4] == '0000':
                 check_proof = True
             else:
                 new_proof+=1
                 
         return new_proof
+    
+    def hash(self, block):
+       encoded_block = json.dumps(block, sort_keys = True).encode()
+       return hashlib.sha256(encoded_block).hexdigest()
+   
+    def is_chain_valid(self, chain):
+        
 
 # Part 2 - Mining our Blockchain
 
